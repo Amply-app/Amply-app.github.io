0b85a1972f0f8149a7689fdb03d2f3a91f27311b
Amply-app <admin@go-amply.com>
2024-09-22 23:04:51 +0200

    Move static files to root for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    output: 'export',
+    basePath: '/Amply-app', // Reemplaza 'nombre-repositorio' con el nombre de tu repositorio
+    trailingSlash: true, // Agrega un slash al final de las rutas
+  }
